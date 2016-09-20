4dfb5066d4a27433e93e35b41d3819043a41993f
Mark Goho <markgoho@gmail.com>
2016-09-20 12:42:47 -0400

    Adding in mixins for arrows

--- js/app.js
+++ js/app.js
@@ -1,6 +1,11 @@
 //When user presses the arrow, other list items will either show or hide depending on current state
 
-$(".header-nav-arrow").click(function() {
-
-	console.log("User clicked the arrow!");
-})
+$(".header-nav-arrow").click(function() { //user clicks the nav arrow list item
+	if ($(this).hasClass("open")) {  // if it's open, change the arrow to "closed" and hide the other list items
+		$(this).removeClass("open").addClass("closed");
+		$(".header-navigation li:nth-child(n+2)").hide();
+	} else {
+		$(this).removeClass("closed").addClass("open"); // if it's closed, change the arrow to "open" and show the other list items
+		$(".header-navigation li:nth-child(n+2)").show();
+	}
+});
