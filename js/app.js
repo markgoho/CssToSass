446f8fdd8a516e5988b33c67de14fd4b9e7e4be4
Mark Goho <markgoho@gmail.com>
2016-09-20 12:11:34 -0400

    Finished basic requirements

---
+++ js/app.js
@@ -0,0 +1,6 @@
+//When user presses the arrow, other list items will either show or hide depending on current state
+
+$(".header-nav-arrow").click(function() {
+
+	console.log("User clicked the arrow!");
+})
